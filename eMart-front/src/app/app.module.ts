0c20b3c064ed9fe6ff2252ac53f570bb1d8f5b56
mumbb <mumbb@cn.ibm.com>
2020-05-02 16:37:26 +0800

    page

--- eMart-front/src/app/app.module.ts
+++ eMart-front/src/app/app.module.ts
@@ -1,18 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserLoginComponent } from './user-login/user-login.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ListCosmeticsComponent } from './list-cosmetics/list-cosmetics.component';
+import { ListGirlsComponent } from './list-girls/list-girls.component';
+import { ListMaternityComponent } from './list-maternity/list-maternity.component';
+import { ListWomenComponent } from './list-women/list-women.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      UserLoginComponent,
+      TopBarComponent,
+      ListWomenComponent,
+      ListMaternityComponent,
+      ListGirlsComponent,
+      ListCosmeticsComponent,
+      ProductDetailsComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule,
+      RouterModule.forRoot([
+         { path: '', component: AppComponent },
+         { path: 'login', component: UserLoginComponent },
+         { path: 'women', component: ListWomenComponent },
+         { path: 'maternity', component: ListMaternityComponent },
+         { path: 'girls', component: ListGirlsComponent },
+         { path: 'cosmetics', component: ListCosmeticsComponent },
+         { path: 'products/:productId', component: ProductDetailsComponent },
+   ]),
+],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
