f4d3800917df6e454ab857b5379aa3789ea48a52
mumbb <mumbb@cn.ibm.com>
2020-06-02 22:41:16 +0800

    mockito

---
+++ eMart-back/item-service/src/test/java/com/ibm/emart/item/service/ItemServiceTest.java
@@ -0,0 +1,43 @@
+package com.ibm.emart.item.service;
+
+import static java.lang.Boolean.FALSE;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.ibm.emart.item.repository.ItemRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+class ItemServiceTest {
+
+  @Mock
+  ItemRepository itemRepository;
+
+  @InjectMocks
+  ItemServiceImp itemServiceImp;
+
+  @Test
+  void newitem() {
+    when(itemRepository.getList().isEmpty())
+    .thenReturn(FALSE);
+    assertEquals(FALSE, itemServiceImp.getList());
+
+  }
+
+  @Test
+  void getList() {
+  }
+
+  @Test
+  void delete() {
+  }
+
+  @Test
+  void updateitem() {
+  }
+}
