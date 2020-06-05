1f4a25e6835d241a817aa7ff7de2356c30cb7e84
mumbb <mumbb@cn.ibm.com>
2020-06-06 07:29:51 +0800

    zuul

--- eMart-back/item-service/src/main/java/com/ibm.emart.item/controller/Controller.java
+++ eMart-back/item-service/src/main/java/com/ibm.emart.item/controller/Controller.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,6 +17,11 @@ public class Controller {
 
   @Autowired
   ItemService itemService;
+
+  @GetMapping("/index")
+  public String index() {
+    return "index";
+  }
 
   @PostMapping("/newitem")
   @CrossOrigin(origins = "http://localhost:4200",allowCredentials = "true")
